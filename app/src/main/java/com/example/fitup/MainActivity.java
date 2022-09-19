188488bb05d35adcb7d64cfbbe6f708e8e034934
Fedor <shmakov987@mail.ru>
2022-09-19 16:27:06 +0300

    Рзработка начального приветствующего экрана

--- app/src/main/java/com/example/fitup/MainActivity.java
+++ app/src/main/java/com/example/fitup/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.fitup;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.*;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.ButtonLesGo);
+
+    }
+
+    public void onClickStart(View view) {
+
     }
 }
