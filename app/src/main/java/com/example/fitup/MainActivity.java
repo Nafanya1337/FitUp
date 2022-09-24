71f958140dcbb8493d0853c21eb3bb1494bfa7f2
Fedor <shmakov987@mail.ru>
2022-09-25 02:40:13 +0300

    Рзработка начального приветствующего экрана

--- app/src/main/java/com/example/fitup/MainActivity.java
+++ app/src/main/java/com/example/fitup/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.fitup;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelStore;
 
 import android.content.Intent;
 
@@ -12,35 +10,15 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView start;
     SharedPreferences prefs = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        start = findViewById(R.id.rectangle_5);
-        prefs = getSharedPreferences("com.mycompany.myAppName", MODE_PRIVATE);
-        if(prefs.getBoolean("firstrun",true))
-            start.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this, Step1.class);
-                    startActivity(intent);
-                }
-            });
-        else
-        {
-            Intent intent = new Intent(MainActivity.this, home.class);
-            startActivity(intent);
-        }
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (prefs.getBoolean("firstrun", true)) {
-            prefs.edit().putBoolean("firstrun", false).commit();
+        prefs = getSharedPreferences("com.mycompany.appname", MODE_PRIVATE);
+        if (prefs.getBoolean("firstrun", true)){
+                Intent intent = new Intent(MainActivity.this, welcome.class);
+                startActivity(intent);
         }
     }
 }
--- app/src/main/java/com/example/fitup/welcome.java
+++ app/src/main/java/com/example/fitup/welcome.java
@@ -3,26 +3,24 @@ package com.example.fitup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class step4 extends AppCompatActivity {
+public class welcome extends AppCompatActivity {
 
     ImageView start;
-    SharedPreferences prefs = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_step4);
-        start = findViewById(R.id.next4);
-        prefs = getSharedPreferences("com.mycompany.appname", MODE_PRIVATE);
+        setContentView(R.layout.activity_welcome);
+
+        start = findViewById(R.id.rectangle_5);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(step4.this, MainActivity.class);
-                prefs.edit().putBoolean("firstrun", false).commit();
+                Intent intent = new Intent(welcome.this, Step1.class);
                 startActivity(intent);
             }
         });
