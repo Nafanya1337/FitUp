cfd2d51a0d29cd3420b85dbb618e810d12b8b182
Fedor <shmakov987@mail.ru>
2022-09-25 00:26:12 +0300

    Рзработка начального приветствующего экрана

--- app/src/main/java/com/example/fitup/Step1.java
+++ app/src/main/java/com/example/fitup/Step1.java
@@ -3,25 +3,26 @@ package com.example.fitup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.view.*;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class Step1 extends AppCompatActivity {
+
     ImageView start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        start = findViewById(R.id.rectangle_5);
+        setContentView(R.layout.activity_step1);
+
+        start = findViewById(R.id.next);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Step1.class);
+                Intent intent = new Intent(Step1.this, step2.class);
                 startActivity(intent);
             }
         });
-        }
     }
+}
--- app/src/main/java/com/example/fitup/step4.java
+++ app/src/main/java/com/example/fitup/step4.java
@@ -3,25 +3,25 @@ package com.example.fitup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.view.*;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class step4 extends AppCompatActivity {
+
     ImageView start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        start = findViewById(R.id.rectangle_5);
+        setContentView(R.layout.activity_step4);
+        start = findViewById(R.id.next4);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Step1.class);
+                Intent intent = new Intent(step4.this, home.class);
                 startActivity(intent);
             }
         });
-        }
     }
+}
