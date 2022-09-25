1fa0bbfd9a827f5d50304aac969da6729a7086ba
Fedor <shmakov987@mail.ru>
2022-09-25 04:41:24 +0300

    Сделан начальный экран Разрабатывается меню

--- app/src/main/java/com/example/fitup/MainActivity.java
+++ app/src/main/java/com/example/fitup/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.fitup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import android.content.Intent;
 
@@ -9,16 +13,65 @@ import android.view.*;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.example.fitup.databinding.ActivityMainBinding;
+import com.example.fitup.fragments.FitFragment;
+import com.example.fitup.fragments.FoodFragment;
+import com.example.fitup.fragments.HomeFragment;
+import com.example.fitup.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
     SharedPreferences prefs = null;
+    ActivityMainBinding binding;
+    BottomNavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         prefs = getSharedPreferences("com.mycompany.appname", MODE_PRIVATE);
-        if (prefs.getBoolean("firstrun", true)){
-                Intent intent = new Intent(MainActivity.this, welcome.class);
-                startActivity(intent);
+        if (prefs.getBoolean("firstrun", true)) {
+            Intent intent = new Intent(MainActivity.this, welcome.class);
+            startActivity(intent);
         }
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setItemIconTintList(null);
+
+            switch (item.getItemId())
+            {
+                case R.id.homeFragment:
+
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profileFragment:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.fitFragment:
+                    replaceFragment(new FitFragment());
+                    break;
+                case R.id.foodFragment:
+                    replaceFragment(new FoodFragment());
+                    break;
+            }
+
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fl_wrapper,fragment);
+        fragmentTransaction.commit();
+    }
+
 }
--- app/src/main/java/com/example/fitup/fragments/FitFragment.java
+++ app/src/main/java/com/example/fitup/fragments/FitFragment.java
@@ -1,4 +1,4 @@
-package com.example.fitup;
+package com.example.fitup.fragments;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.fitup.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
+ * Use the {@link FitFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class FitFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class ProfileFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ProfileFragment() {
+    public FitFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class ProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
+     * @return A new instance of fragment FitFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
+    public static FitFragment newInstance(String param1, String param2) {
+        FitFragment fragment = new FitFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_fit, container, false);
     }
 }
