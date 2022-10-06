5a2eca1135ccff8374fe40ae7644c8f825258eb6
sselesu <horov.kolya@yandex.ru>
2022-10-06 23:19:53 +0300

    6.10

--- app/src/main/java/com/example/fitup/fragments/FitFragment.java
+++ app/src/main/java/com/example/fitup/fragments/FitFragment.java
@@ -1,66 +1,48 @@
 package com.example.fitup.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.fitup.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FitFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
+
+
+
+
 public class FitFragment extends Fragment {
+    private FitFragment fitFragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FitFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FitFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FitFragment newInstance(String param1, String param2) {
-        FitFragment fragment = new FitFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fit, container, false);
+        View root = inflater.inflate(R.layout.fragment_fit, container, false);
+        Button button=(Button) root.findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(),Warmup.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+        return root;
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/fitup/fragments/Warmup.java
@@ -0,0 +1,18 @@
+package com.example.fitup.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fitup.R;
+
+public class Warmup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warmup);
+    }
+
+
+}
