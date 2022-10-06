4f7411114bdd4e357e838e249990fd711f5bba3c
sselesu <horov.kolya@yandex.ru>
2022-10-07 01:39:38 +0300

    7.10

--- app/src/main/java/com/example/fitup/fragments/FitFragment.java
+++ app/src/main/java/com/example/fitup/fragments/FitFragment.java
@@ -4,43 +4,55 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.fitup.R;
-
-
-
-
+import com.example.fitup.databinding.ActivityMainBinding;
 
 
 public class FitFragment extends Fragment {
     private FitFragment fitFragment;
-
+    ImageView warmup_start;
+    ActivityMainBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_fit, container, false);
-        Button button=(Button) root.findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        warmup_start = (ImageView) getView().findViewById(R.id.warmup_start);
+        warmup_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getContext(),Warmup.class);
+                view.startAnimation(AnimationUtils.loadAnimation(getActivity(), R.anim.animation));
+                Fragment fragment = new WarmupF();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
 
-                startActivity(intent);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragmentTransaction.setCustomAnimations(R.animator.my_fade_in, R.animator.my_fade_out);
+                fragmentTransaction.replace(R.id.fr_place, fragment);
+                fragmentTransaction.commit();
             }
         });
+    }
 
-
-        return root;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fit, container, false);
     }
 
 
